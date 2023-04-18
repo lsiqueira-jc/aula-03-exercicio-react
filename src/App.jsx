e3af09f9c30153119808ca2eddb859064866af25
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-04-18 20:27:58 -0300

    ADD components [Header & Posts]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { Exemplo } from './components/Exemplo'
+import Header from './components/Header/Header'
+import Posts from './components/Posts/Posts'
 
 import './Global.css'
+import styles from './App.module.css';
 
 function App() {
+  const url = 'https://via.placeholder.com/500x350'
+  const text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt iste saepe voluptates ad, vitae earum, dolorum consequatur architecto cumque repellendus eius culpa dolores ipsam voluptas officiis deleniti praesentium excepturi magni. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nesciunt dolore accusamus soluta consequuntur ipsum culpa, quia similique delectus autem fugit perspiciatis. Vel, voluptas. Eaque atque maxime quos magnam fugit.'
   return (
     <div>
-      <h1>Hello world</h1>
-      <Exemplo />
+      <Header />
+      <div className={styles.posts}>
+        <Posts
+          url_img={url}
+          text={text}
+        />
+        <Posts
+          url_img={url}
+          text={text} />
+        <Posts
+          url_img={url}
+          text={text} />
+      </div>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css';
+import logo from '../../assets/ne10_logo.png'
+
+const Header = () => {
+  return (
+    <div className={styles.box}>
+      <img src={logo} alt="Logo do Ne10" className={styles.img} />
+    </div>
+  )
+}
+
+export default Header

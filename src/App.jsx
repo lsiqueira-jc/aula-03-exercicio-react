9206529f7ce8ae37a84aea4b54b7237cf0005872
Thaís Bandeira <thaisserpa24@gmail.com>
2023-05-16 18:59:12 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { Exemplo } from './components/Exemplo'
+import Post from "./components/Post";
 
-import './Global.css'
+import "./Global.css";
 
-function App() {
+const ultimasNoticias = [
+  {
+    id: 1,
+    imagem:
+        "https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2023/05/16/597x330/1_water_tap_gc263c76be_1920-22994562.jpg",
+    descricao:
+      "Bairros do Grande Recife vão enfrentar suspensão no abastecimento de água, anuncia Compesa",
+  },
+  {
+    id: 2,
+    imagem:
+      "https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2022/05/13/806x444/1_ds12052022_1__28_-21014105.jpg",
+    descricao:
+      "MELHORES UNIVERSIDADES DO BRASIL: UFPE é a melhor do Nordeste, mas fica longe do top 3 nacional em ranking",
+  },
+  {
+    id: 3,
+    imagem:
+      "https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2023/05/15/806x444/1_img_2495-22993563.jpg",
+    descricao:
+      "No dia do Gari, sindicato destaca pautas prioritárias da categoria e presta homenagem ao gari assassinado em Boa Viagem",
+  },
+];
+
+export default function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <Exemplo />
-    </div>
-  )
-}
+      <h1>Notícias Recentes</h1>
 
-export default App
+      {ultimasNoticias.map((post) => {
+        return <Post imagem={post.imagem} descricao={post.descricao} />;
+      })}
+    </div>
+  );
+}
